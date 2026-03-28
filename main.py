b09c4c9955eec3d7a3f8501b9fe0d0449729f513
tanatchacho-cmd <tanatcha.cho@gmail.com>
2026-03-28 14:00:42 +0700

    Now I Know

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 # print hello world 
-print("Hello World")
-
-# print good bye
-print ("Good Bye")
-
-# new function
-def add_two(num):
-  return num+2
-
-print(add_two(2), add_two(5))
+print("Now I know")
